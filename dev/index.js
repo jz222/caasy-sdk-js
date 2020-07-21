e902b8f7980f86bb6c80f6e52cf70ae2aceb6af1
Timo <timotheus.zimmermann@me.com>
2020-07-21 15:44:58 +0200

    add error handling and comments

--- dev/index.js
+++ dev/index.js
@@ -1,22 +1,22 @@
 const caasy = require('../dist/caasy-min');
 
-caasy.init({ siteId: '5f06fbec5f52c3514a17c329', mode: 'dev' })
+caasy.init({ siteId: '5f05b799985d5565e1826922' })
 
 const main = async () => {
     try {
         const allPosts = await caasy.posts.getAll();
         console.log(allPosts);
         
-        const post = await caasy.posts.getById('5eea4200be133a05fe606f84');
-        console.log(post);
-        
+        const post = await caasy.posts.getById('5f11a482b37775863c3243a8');
+        console.log(post.getElementById('1594991894531-636n'));
+
         const allPostIds = await caasy.posts.getAllIds();
         console.log(allPostIds);
-        
+
         const allAuthors = await caasy.posts.getAllAuthors(2);
         console.log(allAuthors);
-        
-        const postsByAuthor = await caasy.posts.getAllByAuthor(2);
+
+        const postsByAuthor = await caasy.posts.getAllByAuthor('5f05b66d985d5565e1826921', 1);
         console.log(postsByAuthor);
         
     } catch (error) {

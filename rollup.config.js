24e067a746c4510d93ec7f1ef6fdcb21fedca099
Timo <timotheus.zimmermann@me.com>
2020-07-27 15:01:53 +0200

    replace HTTP client

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,5 @@
 import babel from 'rollup-plugin-babel';
 import { terser } from 'rollup-plugin-terser';
-// import { nodeResolve } from '@rollup/plugin-node-resolve';
-// import commonjs from '@rollup/plugin-commonjs';
-// import builtins from 'rollup-plugin-node-builtins';
-
 
 export default {
     input: 'lib/index.js',
@@ -13,22 +9,9 @@ export default {
         name: 'caasy'
     },
     plugins: [
-        // nodeResolve(),
-        // commonjs(),
-        // builtins(),
         babel({
             extensions: ['.js'],
             exclude: ['node_modules/@babel/**', 'node_modules/core-js/**'],
-            // presets: [
-            //     [
-            //         '@babel/preset-env',
-            //         {
-            //             useBuiltIns: 'usage',
-            //             debug: true,
-            //             corejs: 3
-            //         }
-            //     ]
-            // ]
         }),
         terser()
     ]
